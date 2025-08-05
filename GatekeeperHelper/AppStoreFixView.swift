8bd2605b07f1f6eed895e897d0849e4e072c314e
Tang Ziyao <tangeffort@outlook.com>
2025-08-05 20:24:12 +0800

    .

--- GatekeeperHelper/AppStoreFixView.swift
+++ GatekeeperHelper/AppStoreFixView.swift
@@ -29,11 +29,11 @@ struct AppStoreFixView: View {
 
             ScrollView {
                 VStack(alignment: .leading, spacing: 14) {
-                    Text("这是由于Mac OS的软件安装安全性设置默认是仅允许安装来自「App Store 」的软件，进入设置调整修改即可。")
+                    Text("这是由于Mac OS的软件来源安全性设置默认是仅允许安装来自「App Store 」的软件，进入设置调整修改即可。")
 
                     Text("解决方式如下：")
 
-                    Text("1. 打开「系统设置」→「隐私与安全性」。\n2. 滚动到底部，在“安全性-允许以下来源的应用程序”处选择“App Store与已知开发者”后输入密码确认修改。\n3. 完成调整后即可运行相应App。\n\n你也可通过本软件第一个App启动问题中的“永久禁用Gatekeeper”功能，获得把该设置改为“任何来源”的权限。否则，Apple出于安全考虑不展示此选项。")
+                    Text("1. 打开「系统设置」→「隐私与安全性」。\n2. 滚动到底部，在“安全性-允许以下来源的应用程序”处选择“App Store与已知开发者”后输入密码确认修改。\n3. 完成调整后即可运行相应App。\n\n你也可通过本软件第一个App启动问题中的“永久禁用Gatekeeper”功能，获得把该设置改为“任何来源”的权限。否则，Apple出于安全考虑,不会直接展示此选项。")
 
                     Rectangle()
                         .fill(Color.gray.opacity(0.1))
